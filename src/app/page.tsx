186ca1782988368bd65dc04529224ac416ddae6c
Rishi Banerjee <rishibanerjee10@gmail.com>
2025-02-19 15:29:03 +0530

    Added logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import Link from "next/link";
 import { ArrowRight } from "lucide-react";
 import { buttonVariants } from "@/components/ui/button";
 import Image from 'next/image';
+import { Icons } from "@/components/Icons";
 
 export default function Home() {
   return (
     <>
       <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
         <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
+          <Icons.logo className="h-6 w-6" />
           <p className="text-sm font-semibold text-gray-700">
             BookWorm is now Public!
           </p>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,13 +3,15 @@ import MaxWidthWrapper from "./MaxWidthWrapper"
 import { buttonVariants } from "./ui/button"
 import { LoginLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs/server'
 import { ArrowRight } from "lucide-react"
+import { Icons } from "./Icons"
 
 const Navbar = () => {
     return (
         <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
             <MaxWidthWrapper>
                 <div className="flex h-14 items-center justify-between border-b border-zinc-200">
-                    <Link href='/' className="flex z-40 font-semibold">
+                    <Link href='/' className="flex z-40 font-semibold justify-center items-center gap-4">
+                        <Icons.logo className="h-9 w-9" />
                         <span>BookWorm</span>
                     </Link>
 
