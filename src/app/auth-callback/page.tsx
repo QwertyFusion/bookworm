94c31a3243db6ac6959f567a6d6ed011598de569
Rishi Banerjee <rishibanerjee10@gmail.com>
2025-02-19 20:18:17 +0530

    Fixed deployment error

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,46 +1,54 @@
-"use client"
+"use client";
+
+import { Suspense } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { trpc } from "../_trpc/client";
+import { Loader2 } from "lucide-react";
 
 export const dynamic = "force-dynamic";
 
-import { useRouter, useSearchParams } from 'next/navigation';
-import { trpc } from '../_trpc/client';
-import { Loader2 } from 'lucide-react';
+const AuthCallbackHandler = () => {
+  console.log("auth callback");
 
-const Page = () => {
-    console.log("auth callback")
   const router = useRouter();
   const searchParams = useSearchParams();
-  const origin = searchParams.get('origin');
+  const origin = searchParams.get("origin");
 
   // Use the query hook correctly
   const { data, error } = trpc.authCallback.useQuery(undefined, {
     retry: true,
-    retryDelay: 500, //checking every 0.5s
+    retryDelay: 500, // checking every 0.5s
   });
 
   // Handle success and error states
   if (data && data.success) {
     // User is synced to db
-    router.push(origin ? `/${origin}` : '/dashboard');
+    router.push(origin ? `/${origin}` : "/dashboard");
   }
 
   if (error) {
-    if (error.data?.code === 'UNAUTHORIZED') {
-      router.push('/sign-in');
+    if (error.data?.code === "UNAUTHORIZED") {
+      router.push("/sign-in");
     }
   }
 
   return (
-    <div className='w-full mt-24 flex justify-center'>
-      <div className='flex flex-col items-center gap-2'>
-        <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
-        <h3 className='font-semibold text-xl'>
-          Setting up your account...
-        </h3>
+    <div className="w-full mt-24 flex justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+        <h3 className="font-semibold text-xl">Setting up your account...</h3>
         <p>You will be redirected automatically.</p>
       </div>
     </div>
   );
-}
+};
+
+const Page = () => {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <AuthCallbackHandler />
+    </Suspense>
+  );
+};
 
 export default Page;
